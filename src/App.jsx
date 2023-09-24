712d8a01f363d71c83a8e19a50d7beaae4056864
SAwandya <sachilaawandya@gmail.com>
2023-09-24 13:04:30 +0530

    Make basic layout of the site

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+
+    element: <h1>Home</h1>
+  },
+
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h1>about page</h1>
+      </div>
+    ),
+  }
+
+])
+
+const App = () => {
+
+  return (
+
+    
+    <RouterProvider router={router}/>
+      
+    
+  );
+}
+
+export default App;
